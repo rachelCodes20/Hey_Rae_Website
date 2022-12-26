61ea01bc4dc3404a6fde0a82479c8b125442b2b5
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-26 16:55:11 -0500

    configuration, addition index js for main files + component file

---
+++ src/components/Layout/index.js
@@ -0,0 +1,11 @@
+import '../index.scss';
+import { Routes } from 'react-router-dom'
+import SideBar from '../Sidebar'
+
+const Layout = () => {
+    return <>
+    <SideBar/>
+    </>
+}
+
+export default Layout
