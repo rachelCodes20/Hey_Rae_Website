3ab90f9044cb53dde1eb23c1e079a17b890731d3
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-28 20:46:12 -0500

    source

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './components/Home';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
+import NavBar from './components/NavBar';
+import OpenSource from './components/OpenSource';
 
 function App() {
   return (
     <div className="root">
       <Router>
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/opensource" element={<OpenSource />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/experience" element={<Experience />} />
         </Routes>
