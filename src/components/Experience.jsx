83a7f8b1524233fb39fff6667e8872f1e6feacb4
Rachel Collins <spiritedndiligent@gmail.com>
2022-12-31 00:33:48 -0500

    adding timeline

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { 
+VerticalTimeline, 
+VerticalTimelineElement }
+from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import SchoolIcon from '@mui/icons-material'; 
 
 function Experience() {
-  return <div>Experience</div>;
+  return (
+  <div className='experience'>
+    
+    <VerticalTimeline lineColor='#3e497a'>
+      <VerticalTimelineElement className='vertical-timeline-element--education'
+      date='2003 - 2014'
+      iconStyle={{background:"#3e497a", color: '#fff'}}
+      icon={<SchoolIcon />}
+      ></VerticalTimelineElement>
+      </VerticalTimeline></div>
+  );
 }
 
 export default Experience;
